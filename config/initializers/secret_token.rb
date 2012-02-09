07255390e71a3f6af930be5976f14355e098be82
Alberto Gonzalez <darkxer0x@gmail.com>
2012-02-09 20:42:52 +0100

    new planet creado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planet::Application.config.secret_token = '99e021b357777422c703bc9b826ee8d5bee23385f1d55fab0b354bdfc82b51375eec0adaa24a7be860dd8bdb3491f9f882782d48363f715130faf51e1d172b5b'
